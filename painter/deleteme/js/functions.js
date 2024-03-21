b84c5273c48fcaf693cb67106220ee7d5c8f64e2
Sirjoeroe <127991439+Sirjoeroe@users.noreply.github.com>
2024-03-22 00:30:01 +0200

    Add files via upload

--- painter/deleteme/js/functions.js
+++ painter/deleteme/js/functions.js
@@ -1,4 +1,7 @@
-import { Circle } from "./class/Circle.js"
+import { Circle } from "./class/Circle.js";
+import { Line } from "./class/Line.js";
+import { Rectangle } from "./class/Rectangle.js";
+import { Square } from "./class/Square.js";
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
@@ -8,7 +11,7 @@ const draw_button = document.querySelector('button')
 const radio_form = document.querySelector('form')
 const line_width_input = document.querySelector('input[type=number]')
 
-const updateUI = (label3,label4)=> {
+const updateUI = (label3, label4) => {
     document.querySelector('div#third label').innerHTML = label3
 
     if (label4 !== undefined) {
@@ -21,39 +24,39 @@ const updateUI = (label3,label4)=> {
     }
 }
 
-radio_form.addEventListener('click',() => {
+radio_form.addEventListener('click', () => {
     const shape = radio_form['shape'].value
     switch (shape) {
         case 'line':
-            updateUI('x2', 'y2')
+            updateUI('X2:', 'Y2:')
             break
         case 'circle':
-            updateUI('width:')
+            updateUI('Width:')
             break
         case 'rectangle':
-            updateUI('width:','height:')
+            updateUI('Width:', 'Height:')
             break
         case 'square':
-            updateUI('width:')
+            updateUI('Width:')
             break
     }
 })
 
-draw_button.addEventListener('click', () =>{
+draw_button.addEventListener('click', () => {
     const shape = radio_form['shape'].value
 
     switch (shape) {
         case 'line':
-
-            brake
+            drawLine()
+            break
         case 'circle':
             drawCircle()
             break
         case 'rectangle':
-
+            drawRectangle()
             break
         case 'square':
-
+            drawSquare()
             break
     }
 })
@@ -63,7 +66,39 @@ const drawCircle = () => {
     const y = Y_input.value
     const width = document.querySelector('div#third input').value
     const lineWidth = line_width_input.value
-    const circle = new Circle(x,y,width/2)
+    const circle = new Circle(x, y, width / 2)
     circle.setLineWidth = lineWidth
     circle.draw(ctx)
+}
+
+const drawLine = () => {
+    const x1 = x_input.value
+    const y1 = Y_input.value
+    const x2 = document.querySelector('div#third input').value
+    const y2 = document.querySelector('div#fourth input').value
+    const lineWidth = line_width_input.value
+    const line = new Line(x1, y1, x2, y2)
+    line.setLineWidth = lineWidth
+    line.draw(ctx)
+}
+
+const drawRectangle = () => {
+    const x = x_input.value
+    const y = Y_input.value
+    const width = document.querySelector('div#third input').value
+    const height = document.querySelector('div#fourth input').value
+    const lineWidth = line_width_input.value
+    const rectangle = new Rectangle(x, y, width, height)
+    rectangle.setLineWidth = lineWidth
+    rectangle.draw(ctx)
+}
+
+const drawSquare = () => {
+    const x = x_input.value;
+    const y = Y_input.value;
+    const sideLength = document.querySelector('div#third input').value;
+    const lineWidth = line_width_input.value;
+    const square = new Square(x, y, sideLength);
+    square.setLineWidth = lineWidth
+    square.draw(ctx);
 }
